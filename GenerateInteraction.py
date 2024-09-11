e77261fd9db63687c2e85cb52dae046a7d420c6f
“Shirley” <ShirleyHuang7@hotmail.com>
2024-09-11 15:38:57 -0400

    Scripted a realistic simulation of customer interaction for Matrix Factorization

--- GenerateInteraction.py
+++ GenerateInteraction.py
@@ -10,26 +10,43 @@ def generateInteraction(filename):
 
     print(os.getcwd())
 
-
     #Open CSV file and write content
     with open(filename, 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(headers)
+
+        non_zero_item = 0
+        total_reviews = users * menu
         
-        #For each 100 customers, they must give review to all the menus (Making data sparse)
         for customer in range(users):
-            randomNum= 150
             reviewed_items=set()
+
+            numReviews = random.randint(50,150)    #Each customer should rate 10 or more item
+            numPreference = random.randint(5, numReviews//2)    #Each customer will have preference for 1 item or up to 1/2 the item they review
+            preference_bias = random.sample(range(1, menu+1), k= numPreference)    
+
             
-            for _ in range(randomNum):
-                randomfood = random.randint(1,menu)
+            for _ in range(numReviews):
+                randomfood = random.randint(1, menu)
 
                 if randomfood not in reviewed_items:
-                    randomrating = random.randint(1,5)
+
+                     #Rate that item is a part of their preference, rank them higher
+                    if randomfood in preference_bias:
+                        randomrating = random.randint(4,5)  
+                    else:
+                       randomrating = random.randint(1,5)
+                
                     writer.writerow([customer+1, randomfood, randomrating])
                     reviewed_items.add(randomfood)
+                    non_zero_item +=1
 
-            print(f"Customer {customer+1} - {reviewed_items}")
+            print(f"Customer {customer+1} - {reviewed_items} (PREFERENCES: {preference_bias})")
+
+        #DATA SPARSITY: Where items have not been reviewed by a customer - missing from dataset
+        sparsity = 1 - (non_zero_item/total_reviews)
+        print(f"Data Sparsity: {sparsity * 100:.2f}%")
+
 
 if __name__ == '__main__':
     generateInteraction('/Users/shirleyhuang/Documents/Apps/RecommendIQ/User_Data.csv')
